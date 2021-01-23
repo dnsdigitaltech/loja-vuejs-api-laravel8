91d3e1745931f70bc706c6ace6716e1dbe44c445
dnsdigitaltech <dnssites@gmail.com>
2021-01-23 19:42:11 -0300

    Criando Projeto Vue CLI = Formulario

---
+++ forms-vue/src/components/FormComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        
+        <form action="">
+            <input type="text" name="" placeholder="Nome">
+            <hr>
+            <input type="email" name="" placeholder="E-mail">
+            <hr>
+            <input type="number" name="" placeholder="Idade">
+            <hr>
+            <input type="radio" name="sex"  value="M"> Masculino
+            <input type="radio" name="sex"  value="F"> Feminino
+            <hr>
+            <select name="" id="">
+                <option value="">Selecione o Estado</option>
+                <option value="SP">São Paulo</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="MG">Minas Gerais</option>
+            </select>
+            <hr>
+            <label for="agree">Concordo com os termos de uso</label>
+            <input type="checkbox" name="" id="agree">
+            <hr>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <hr>
+            <button type="submit">Enviar agora</button>
+        </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
