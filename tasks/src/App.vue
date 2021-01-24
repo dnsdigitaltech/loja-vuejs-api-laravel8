5ecbd7bb3720a6578f6212118128795c2107646d
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 08:57:45 -0300

    Preparando Projeto Lista de Tarefas no Vue JS

---
+++ tasks/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <list-tasks/>
+  </div>
+</template>
+
+<script>
+import ListTasks from './components/Tasks/ListTasks'
+export default {
+  name: 'app',
+  components: {
+    ListTasks
+  }
+}
+</script>
+
+<style>
+</style>
