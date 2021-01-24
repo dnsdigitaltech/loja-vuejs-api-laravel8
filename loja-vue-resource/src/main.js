53a3b15eafe3c3ff17100ad0c9a642173d10883e
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 17:51:32 -0300

    Preparar Projeto Consumir API Restful em Laravel com Vue JS

---
+++ loja-vue-resource/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueRecource from 'vue-resource'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.use(VueRouter)
+Vue.use(VueRecource)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
