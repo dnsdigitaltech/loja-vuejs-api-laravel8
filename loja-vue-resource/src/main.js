ef18aa437cfce6e6ada33660b6a9e1b5fd33aad0
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 18:13:31 -0300

    Preparar Rotas no Vue JS

--- loja-vue-resource/src/main.js
+++ loja-vue-resource/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueRecource from 'vue-resource'
+import routes from './routes/index'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -13,7 +14,13 @@ Vue.use(IconsPlugin)
 Vue.use(VueRouter)
 Vue.use(VueRecource)
 
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ loja-vue-resource/src/routes/index.js
@@ -0,0 +1,7 @@
+import HomeComponent from '../components/home/HomeComponent'
+import ProductsComponent from '../components/products/ProductsComponent'
+
+export default [
+    {path: '/', component: HomeComponent},
+    {path: '/Produtos', component: ProductsComponent}
+]
