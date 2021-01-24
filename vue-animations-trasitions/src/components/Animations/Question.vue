0d46a7b4e46bc58ed4ac10034a212a3634371b6e
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 14:28:38 -0300

    Gerar Pergunta Quiz Dinamicamente no Vue JS

--- vue-animations-trasitions/src/components/Animations/Question.vue
+++ vue-animations-trasitions/src/components/Animations/Question.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h2 v-text="question"></h2>
+        <form @submit.prevent="checkResult">
+            <input type="text" name="" placeholder="Resposta" v-model="reply">
+            <button type="submit">Responder</button>
+        </form>
     </div>
 </template>
 
@@ -8,7 +12,31 @@
     export default {
         data() {
             return {
-                question: 'Gerando pergunta...'
+                question: 'Gerando pergunta...',
+                result: 0,
+                reply: ''
+            }
+        },
+        created() {
+            this.generateQuestion()
+        },
+        methods: {
+            generateQuestion(){
+                let numberOne = this.generateRandomNumber(1, 100)
+                let numberTwo = this.generateRandomNumber(1, 100)
+
+                this.result = numberOne + numberTwo
+
+                this.question = `Qual é o resultado da soma entre ${numberOne} e ${numberTwo}?`
+            },
+            generateRandomNumber(min, max){
+                return Math.round(Math.random() *  (max - min)) + min
+            },
+            checkResult() {
+                if (this.reply == this.result)
+                    alert('Acertou')
+                else
+                    alert('errou')
             }
         },
     }
