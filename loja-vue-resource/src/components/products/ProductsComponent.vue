8605b4d083e4c398cc0b4b60d4c2c49a017eb761
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 18:34:31 -0300

    Listar Itens API Restful em Laravel no Vue JS

--- loja-vue-resource/src/components/products/ProductsComponent.vue
+++ loja-vue-resource/src/components/products/ProductsComponent.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
         <h2>{{ title }}</h2>
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th>#id</th>
+                    <th>Nome</th>
+                    <th>Decrição</th>
+                    <th width="200">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(product, index) in products.data" :key="index">
+                    <td>{{ product.id }}</td>
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.description }}</td>
+                    <td>
+                        <a href="#" class="btn btn-info">Editar</a>
+                        <a href="#" class="btn btn-danger">Deletar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -8,7 +29,21 @@
 export default {
     data() {
         return {
-            title: 'Lista de Produtos'
+            title: 'Lista de Produtos',
+            products: {}
+        }
+    },
+    created() {
+        this.getProducts()
+    },
+    methods: {
+        getProducts(){
+            this.$http.get('http://localhost:8000/api/v1/products/')
+            .then(response => {
+                this.products = response.body
+            },error => {
+                console.log(error)
+            })
         }
     },
 }
