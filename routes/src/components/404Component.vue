68826bfe8d079bc018b7e54e0ab7f9a9184c8ca3
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 12:37:32 -0300

    Rotas 404 no Vue JS

---
+++ routes/src/components/404Component.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Error 404'
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/src/routes.js
+++ routes/src/routes.js
@@ -1,10 +1,12 @@
 import HomeComponent from './components/HomeComponent'
 import ProductsComponent from './components/ProductsComponent'
 import TasksComponent from './components/TasksComponent'
+import E404Component from './components/404Component'
 
 export default [
     {path: '/', component: HomeComponent},
     {path: '/produtossdfgsdf ', component: ProductsComponent, name: 'products'},
     {path: '/tarefa/:id', component: TasksComponent, name: 'task', props:true},
-    {path: '/redirect', redirect: {name: 'products'}}
+    {path: '/redirect', redirect: {name: 'products'}},
+    {path: '*', component: E404Component}
 ]
