47c9ab8fd99ce9ab44f387d03bb264e82bf1c5ee
dnsdigitaltech <dnssites@gmail.com>
2021-01-24 16:25:14 -0300

    Consultar CEP com AJAX (resource) no ViaCEP com Vue JS

---
+++ vue-resource-project/src/components/SearchCepComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2 v-text="title"></h2>
+        <form @submit.prevent="onSubmit">
+            <input type="text" name="" placeholder="Informe o CEP" v-model="cep">
+            <button type="submit">Buscar CEP</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Busca CEP com Vue JS',
+            cep: ''
+        }
+    },
+    methods: {
+        onSubmit(){
+            this.$http.get('https://viacep.com.br/ws/'+ this.cep +'/json/')
+                        .then(response => {
+                            console.log(response.body)
+                        }, error => console.log(error))
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
