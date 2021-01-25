d65dd135fc1634bff6b6f33d58a7e63d4e09e440
Davi Bernardo <dnssites@gmail.com>
2021-01-25 16:31:17 -0300

    Vuex mutations commit

--- curso-vuex/src/components/tasks/TaskComponent.vue
+++ curso-vuex/src/components/tasks/TaskComponent.vue
@@ -3,7 +3,12 @@
     <h2>{{ title }}</h2>
     <task-add-component></task-add-component>
     <ul>
-      <li v-for="(task, index) in getTasks" :key="index">
+      <li
+        v-for="(task, index) in getTasks"
+        :key="index"
+        :class="{ 'completed': task.completed }"
+      >
+        <a href="#" @click.prevent="completedTask(task)">Toogle</a>
         {{ task.name }}
       </li>
     </ul>
@@ -23,10 +28,19 @@ export default {
       return this.$store.state.tasks;
     },
   },
+  methods: {
+    completedTask(task) {
+      this.$store.commit("TOOGLE_TASK", task);
+    },
+  },
   components: {
     TaskAddComponent,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.completed {
+  background: green;
+}
+</style>
