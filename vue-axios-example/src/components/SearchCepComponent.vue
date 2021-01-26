aca3eeba76600b57b79f99992646761de2a8ca0b
Davi Bernardo <dnssites@gmail.com>
2021-01-26 11:58:36 -0300

    Preloader com axios no Vue JS

--- vue-axios-example/src/components/SearchCepComponent.vue
+++ vue-axios-example/src/components/SearchCepComponent.vue
@@ -5,6 +5,9 @@
       <input type="text" placeholder="CEP:" v-model="cep" />
       <button type="submit">Buscar</button>
     </form>
+    <div v-if="preloader">
+      <img src="../assets/preloader.gif" class="preloader" alt="" />
+    </div>
     <div v-if="address.cep">
       <p>Cep: {{ address.cep }}</p>
       <p>Logradouro: {{ address.logradouro }}</p>
@@ -23,10 +26,12 @@ export default {
       title: "Buscar CEP",
       cep: "",
       address: {},
+      preloader: false,
     };
   },
   methods: {
     onSubmit() {
+      this.preloader = true;
       let x = axios
         .get(`https://viacep.com.br/ws/${this.cep}/json/`)
         .then((response) => {
@@ -36,10 +41,14 @@ export default {
         .catch((error) => {
           console.log(error);
         })
-        .finally(() => console.log("Finaly"));
+        .finally(() => (this.preloader = false));
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.preloader {
+  max-width: 60px;
+}
+</style>
