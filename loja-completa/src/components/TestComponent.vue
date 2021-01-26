93903d94dbbc2736b4d7980ef58033c6aae6df61
Davi Bernardo <dnssites@gmail.com>
2021-01-26 14:05:33 -0300

    Trabalhando com Vue JS

---
+++ loja-completa/src/components/TestComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Primeiro component em Vue Js",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
