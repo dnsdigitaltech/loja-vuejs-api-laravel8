baa28ee8b6ac28d0a8e8619cec7be241a06f8db3
Davi Bernardo <dnssites@gmail.com>
2021-01-26 13:38:59 -0300

    Tratamento de Erros axios no Vue JS

--- vue-axios-example/src/components/SearchCepComponent.vue
+++ vue-axios-example/src/components/SearchCepComponent.vue
@@ -2,8 +2,11 @@
   <div>
     <h2>{{ title }}</h2>
     <form @submit.prevent="onSubmit">
-      <input type="text" placeholder="CEP:" v-model="cep" />
-      <button type="submit">Buscar</button>
+      <div :class="{ 'has-error': error }">
+        <input type="text" placeholder="CEP:" v-model="cep" />
+        <button type="submit">Buscar</button>
+        <div v-if="error" v-text="error"></div>
+      </div>
     </form>
     <div v-if="preloader">
       <img src="../assets/preloader.gif" class="preloader" alt="" />
@@ -27,6 +30,7 @@ export default {
       cep: "",
       address: {},
       preloader: false,
+      error: "",
     };
   },
   methods: {
@@ -36,10 +40,15 @@ export default {
         .get(`https://viacep.com.br/ws/${this.cep}/json/`)
         .then((response) => {
           console.log(response.data);
-          this.address = response.data;
+          if (response.data.erro) {
+            this.error = "Cep Inválido";
+          } else {
+            this.address = response.data;
+          }
         })
         .catch((error) => {
           console.log(error);
+          this.error = "404";
         })
         .finally(() => (this.preloader = false));
     },
@@ -50,5 +59,11 @@ export default {
 <style scoped>
 .preloader {
   max-width: 60px;
+}
+.has-error {
+  color: rgb(182, 53, 2);
+}
+.has-error input {
+  border: 1px solid rgb(182, 53, 2);
 }
 </style>
