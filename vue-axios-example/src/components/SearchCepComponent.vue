8711e16195a7fa76b2a7ad210cd5c1d20b01198c
Davi Bernardo <dnssites@gmail.com>
2021-01-26 11:52:14 -0300

    Requisições Ajax com axios no Vue JS

--- vue-axios-example/src/components/SearchCepComponent.vue
+++ vue-axios-example/src/components/SearchCepComponent.vue
@@ -5,20 +5,38 @@
       <input type="text" placeholder="CEP:" v-model="cep" />
       <button type="submit">Buscar</button>
     </form>
+    <div v-if="address.cep">
+      <p>Cep: {{ address.cep }}</p>
+      <p>Logradouro: {{ address.logradouro }}</p>
+      <p>Complemento: {{ address.complemento }}</p>
+      <p>Bairro: {{ address.bairro }}</p>
+      <p>Localidade: {{ address.localidade }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       title: "Buscar CEP",
       cep: "",
+      address: {},
     };
   },
   methods: {
     onSubmit() {
-      alert(this.cep);
+      let x = axios
+        .get(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .then((response) => {
+          console.log(response.data);
+          this.address = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => console.log("Finaly"));
     },
   },
 };
