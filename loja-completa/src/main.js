aa40a5fd69f0a55d32ba745c73c96fb0275af73f
Davi Bernardo <dnssites@gmail.com>
2021-01-27 10:45:32 -0300

    Pré-organizar Layout Admin

--- loja-completa/src/main.js
+++ loja-completa/src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './routes/routers'
 import store from './vuex/store'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
   el: '#app',
   router,
