97afb0b84562da214fb5e988bf78e0f1b266843e
Davi Bernardo <dnssites@gmail.com>
2021-01-28 17:05:21 -0300

    Listar Produtos vuex

--- loja-completa/src/components/admin/pages/products/ProductsComponent.vue
+++ loja-completa/src/components/admin/pages/products/ProductsComponent.vue
@@ -5,13 +5,20 @@
       <thead>
         <tr>
           <th>ID</th>
-          <th>IMAGEM</th>
           <th>NOME</th>
+          <th>IMAGEM</th>
+          <th width="200">Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(product, index) in products" :key="index">
           <td>{{ product.id }}</td>
+          <td>{{ product.name }}</td>
+          <td>{{ product.image }}</td>
+          <td>
+            <a href="" class="btn btn-info">Editar</a>
+            <a href="" class="btn btn-danger">Deletar</a>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -29,8 +36,8 @@ export default {
     this.loadProducts();
   },
   computed: {
-    loadProducts() {
-      return this.state.products.items.data.items;
+    products() {
+      return this.$store.state.products.items.data;
     },
   },
   methods: {
