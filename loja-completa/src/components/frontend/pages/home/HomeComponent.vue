ec2fb9bfcf3b2898264412fae3ed093e2009011c
Davi Bernardo <dnssites@gmail.com>
2021-01-29 17:25:03 -0300

    Listar Produtos Home Page Site

--- loja-completa/src/components/frontend/pages/home/HomeComponent.vue
+++ loja-completa/src/components/frontend/pages/home/HomeComponent.vue
@@ -1,29 +1,46 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <div class="row">
+            <div class="col-3" v-for="product in products" :key="product.id">
+            <div v-if="product.image">
+              <img
+                :src="[`${url_img}products/${product.image}`]"
+                :alt="product.name"
+                class="img-list"
+              />
+            </div>
+                {{ product.name }}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { URL_BASE_IMAGE } from '../../../../config/config'
 export default {
     data() {
         return {
-            title: 'Home page',
+            title: 'Produtos',
+            url_img: URL_BASE_IMAGE
         }
     },
     created() {
+        //this.$store.dispatch('loadProducts', {})
         if(this.products.data.length == 0){
             this.$store.dispatch('loadProducts', {})
         }
     },
     computed:{
         products(){
-            return this.$store.state,products.items
+            return this.$store.state.products.items.data
         }
     }
 }
 </script>
 
 <style scoped>
-
+.img-list {
+  max-width: 100px;
+}
 </style>
