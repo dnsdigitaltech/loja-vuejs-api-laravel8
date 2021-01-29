e8e44edbded8c6242aec4c968f32e403dac70716
Davi Bernardo <dnssites@gmail.com>
2021-01-29 18:05:18 -0300

    Paginação de Produtos Home Page com Vue.js

--- loja-completa/src/components/frontend/pages/home/HomeComponent.vue
+++ loja-completa/src/components/frontend/pages/home/HomeComponent.vue
@@ -3,27 +3,30 @@
         <h1>{{title}}</h1>
         <div class="row">
             <div class="col-3" v-for="product in products" :key="product.id">
-            <div v-if="product.image">
-              <img
-                :src="[`${url_img}products/${product.image}`]"
-                :alt="product.name"
-                class="img-list"
-              />
-            </div>
-            <div v-else>
-                <img 
-                :src="[`${url_img}img/noimage.jpg`]" 
-                :alt="product.name"
-                class="img-list">
-            </div>
+                <div v-if="product.image">
+                <img
+                    :src="[`${url_img}products/${product.image}`]"
+                    :alt="product.name"
+                    class="img-list"
+                />
+                </div>
+                <div v-else>
+                    <img 
+                    :src="[`${url_img}img/noimage.jpg`]" 
+                    :alt="product.name"
+                    class="img-list">
+                </div>
                 {{ product.name }}
             </div>
+            <hr>
+            <paginate :pagination="products" :offset="6" @paginate="loadProducts"> </paginate>
         </div>
     </div>
 </template>
 
 <script>
 import { URL_BASE_IMAGE } from '../../../../config/config'
+import PaginationComponent from '../../../layouts/PaginationComponent'
 export default {
     data() {
         return {
@@ -41,6 +44,14 @@ export default {
         products(){
             return this.$store.state.products.items.data
         }
+    },
+    methods: {
+        loadProducts(page = 1) {
+            this.$store.dispatch('loadProducts', {page})
+        }
+    },
+    components: {
+        paginate: PaginationComponent
     }
 }
 </script>
